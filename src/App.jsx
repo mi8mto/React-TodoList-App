d6897558903117f38bfd056b057c9c5c3ec4eddd
Igor Manakhau <mi8mto@gmail.com>
2025-02-04 12:46:25 +0300

    change path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Header } from './App/components/Header/Header';
 import { NotFound404 } from './App/components/NotFound404';
 import { SinglePage } from './App/components/SinglePage';
@@ -10,19 +10,18 @@ import styles from './App.module.css';
 
 export const App = () => {
 	return (
-		<div>
-			{<Header />}
-			<div className={styles.container}>
-				<Routes>
-					<Route path="/React-TodoList-App/" element={<TodoListJsonServer />} />
-					<Route path="/React-TodoList-App/task/:id" element={<SinglePage />} />
-					<Route path="/React-TodoList-App/404" element={<NotFound404 />} />
-					<Route
-						path="*"
-						element={<Navigate to="/React-TodoList-App/404" replace />}
-					/>
-				</Routes>
+		<Router basename="/React-TodoList-App">
+			<div>
+				{<Header />}
+				<div className={styles.container}>
+					<Routes>
+						<Route path="/" element={<TodoListJsonServer />} />
+						<Route path="/task/:id" element={<SinglePage />} />
+						<Route path="/404" element={<NotFound404 />} />
+						<Route path="*" element={<Navigate to="/404" replace />} />
+					</Routes>
+				</div>
 			</div>
-		</div>
+		</Router>
 	);
 };
