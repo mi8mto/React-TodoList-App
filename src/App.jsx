908980a3178065bba811e41f9a7b6573d19d1225
Igor Manakhau <mi8mto@gmail.com>
2025-02-04 14:20:35 +0300

    change router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Header } from './App/components/Header/Header';
 import { NotFound404 } from './App/components/NotFound404';
 import { SinglePage } from './App/components/SinglePage';
@@ -9,18 +9,16 @@ import styles from './App.module.css';
 
 export const App = () => {
 	return (
-		<Router basename="/React-TodoList-App">
-			<div>
-				{<Header />}
-				<div className={styles.container}>
-					<Routes>
-						<Route path="/" element={<TodoListJsonServer />} />
-						<Route path="/task/:id" element={<SinglePage />} />
-						<Route path="/404" element={<NotFound404 />} />
-						<Route path="*" element={<Navigate to="/404" replace />} />
-					</Routes>
-				</div>
+		<div>
+			{<Header />}
+			<div className={styles.container}>
+				<Routes>
+					<Route path="/" element={<TodoListJsonServer />} />
+					<Route path="/task/:id" element={<SinglePage />} />
+					<Route path="/404" element={<NotFound404 />} />
+					<Route path="*" element={<Navigate to="/404" replace />} />
+				</Routes>
 			</div>
-		</Router>
+		</div>
 	);
 };
